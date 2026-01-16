085a6bef2c3206acd4b505ef44d79fb652ac54e4
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-01-16 22:32:21 +0530

    useEffect use

--- Fronthend/src/components/About.jsx
+++ Fronthend/src/components/About.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import { useContext } from 'react'
+import React, { useContext } from 'react'
 import noteContext from '../context/notes/noteContext'
+import { useEffect } from 'react'
 export default function About() {
   const a = useContext(noteContext)
+  useEffect(()=>{
+    a.update()
+  },[])
   return (
     <div>
-     This Is About {a.name}
+     This Is About {a.state.name} and he is in class {a.state.class}
     </div>
   )
 }
