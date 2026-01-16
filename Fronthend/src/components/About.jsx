ca889dc11553b8c66d2e19d47b908f25abdb73c7
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-01-16 22:12:59 +0530

    using noteContext

--- Fronthend/src/components/About.jsx
+++ Fronthend/src/components/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { useContext } from 'react'
+import noteContext from '../context/notes/noteContext'
 export default function About() {
+  const a = useContext(noteContext)
   return (
     <div>
-      This IS ABout
+     This Is About {a.name}
     </div>
   )
 }
