fc66d057c2d98a2a826941f523bb8285ec232bb6
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-01-16 22:32:56 +0530

    Using Set Timeout

--- Fronthend/src/context/notes/NoteState.jsx
+++ Fronthend/src/context/notes/NoteState.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const state = {
+    const s1 = {
         "name": "vinayak",
         "class": "5b"
     }
+    const [state, setState] = useState(s1);
+    const update = () => {
+        setTimeout(() => {
+            setState({
+                "name": "Larry",
+                "class": "6b"
+            })
+        }, 1000)
+    }
     return (
-        <NoteContext.Provider value={state}>
+        <NoteContext.Provider value={{ state, update }}>
             {props.children}
         </NoteContext.Provider>
     )
