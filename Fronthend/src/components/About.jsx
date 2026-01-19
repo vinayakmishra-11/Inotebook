d75cc99f1d3519815d4a0cf65f5c3d743e5c612c
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-01-19 21:48:06 +0530

    Just undo

--- Fronthend/src/components/About.jsx
+++ Fronthend/src/components/About.jsx
@@ -1,14 +1,11 @@
 import React, { useContext } from 'react'
-import noteContext from '../context/notes/noteContext'
+
 import { useEffect } from 'react'
 export default function About() {
-  const a = useContext(noteContext)
-  useEffect(()=>{
-    a.update()
-  },[])
+
   return (
     <div>
-     This Is About {a.state.name} and he is in class {a.state.class}
+     This Is About and he is in class 
     </div>
   )
 }
--- Fronthend/src/context/notes/NoteState.jsx
+++ Fronthend/src/context/notes/NoteState.jsx
@@ -2,21 +2,10 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "vinayak",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1);
-    const update = () => {
-        setTimeout(() => { // just temperarory 
-            setState({
-                "name": "Larry",
-                "class": "6b"
-            })
-        }, 1000)
-    }
+    
+    
     return (
-        <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider value={{ }}>
             {props.children}
         </NoteContext.Provider>
     )
