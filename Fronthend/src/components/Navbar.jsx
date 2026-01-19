aeb0a8a9c5a5fd69e7c128bea60e34e5465f6a20
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-01-19 21:42:32 +0530

    Change the navbar title

--- Fronthend/src/components/Navbar.jsx
+++ Fronthend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Navbar scroll</Link>
+    <Link className="navbar-brand" to="/">iNoteBook</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
