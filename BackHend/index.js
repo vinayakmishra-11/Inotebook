82a91b86ef91bc8fa3c56ba93fb77abb6f0820cb
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-01-20 22:19:27 +0530

    just change console.log in app.listen

--- BackHend/index.js
+++ BackHend/index.js
@@ -9,7 +9,7 @@ app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`inotebook Backhend listening on port ${port}`)
 })
 
 
