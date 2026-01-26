3a695628f955a024562a28a79cbd421ca1413d2e
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-01-26 20:26:22 +0530

    Noteitem.jsx

--- Fronthend/src/components/Noteitem.jsx
+++ Fronthend/src/components/Noteitem.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import "../Styling/Noteitem.css"
+import "../Styling/Noteitem.css";
 import { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
 const Noteitem = (props) => {
-  const { note } = props;
+  const { note, updatedNote } = props;
   const context = useContext(noteContext);
-  const { DeleteNote} = context;
+  const { DeleteNote } = context;
   return (
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
-          <p className="card-text">
-            {note.description} 
-          </p>
-          <i className="fa-solid fa-trash" onClick={()=>{DeleteNote(note._id)}}></i> <i className="fa-regular fa-pen-to-square"></i>
+          <p className="card-text">{note.description}</p>
+          <i
+            className="fa-solid fa-trash"
+            onClick={() => {
+              DeleteNote(note._id);
+            }}
+          ></i>{" "}
+          <i className="fa-regular fa-pen-to-square" onClick={()=>{updatedNote(note)}}></i>
         </div>
-        
       </div>
-      
     </div>
   );
 };
